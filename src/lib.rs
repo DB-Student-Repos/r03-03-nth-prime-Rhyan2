46acca541531408d834523ae4e342bac83c6f1c5
Utman <148863405+Rhyan2@users.noreply.github.com>
2024-04-29 12:08:24 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {n}th prime number?")
+    let mut primes = Vec::new();
+    let mut sieve = vec![true; 1_000_000]; 
+
+    for num in 2..sieve.len() {
+        if sieve[num] {
+            primes.push(num as u32);
+            if primes.len() == (n + 1) as usize {
+                break;
+            }
+            let mut multiple = num * num;
+            while multiple < sieve.len() {
+                sieve[multiple] = false;
+                multiple += num;
+            }
+        }
+    }
+
+    primes[n as usize]
 }
